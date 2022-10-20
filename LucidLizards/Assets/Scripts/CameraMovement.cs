529f9bfa9d610beae3f9e0ff9fa8c6ae4c2c4b90
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2022-10-20 23:34:20 +0800

    Add ice cone, make level easily, change players gravity

--- LucidLizards/Assets/Scripts/CameraMovement.cs
+++ LucidLizards/Assets/Scripts/CameraMovement.cs
@@ -8,7 +8,7 @@ public class CameraMovement : MonoBehaviour
 
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
     }
 
 }
